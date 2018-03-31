c8e451f66918b8e2bb6be4cb27949c57306c6e04
H de Vries <hdevries@fastmail.com>
2018-03-31 19:02:02 +0200

    xss working at findxss/test/?id=

--- findxss/views.py
+++ findxss/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.template import loader, Context
+from django.utils.safestring import mark_safe
+
 
 def index(request):
 	return HttpResponse("Hello world. You're at the polls index.")
 
+def testargument(request):
+	id_query_param = request.GET.get('id')
 
+	template = loader.get_template('findxss/index.html')
+	context = {
+		'id_query_param': id_query_param
+	}
+
+	context2 = {'id_query_param': mark_safe(id_query_param)}
+
+
+	print('context:')
+	print(context)
+
+	# print('query param: ')
+	# print(id_query_param)
+	# return HttpResponse("The following ID is requested: %s" % id_query_param)
+	return HttpResponse(template.render(context2))
 
