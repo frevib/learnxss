666d442840dcd154717c1723517e62628bb77928
Hielke (H.) de Vries <hdevries@fastmail.com>
2018-06-01 14:23:47 +0200

    test

--- findsqlinjection/models.py
+++ findsqlinjection/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Users(models.Model):
 	user_name = models.CharField(max_length=20)
 	password = models.CharField(max_length=20)
+
+	def __str__(self):
+		return self.user_name
